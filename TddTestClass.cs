d5dbc07cdb84df460b656894a0445f54814f7d2f
BENJAMIN NGARAMBE <kukibyanze@gmail.com>
2020-11-06 11:59:02 +0100

    full working program with Tdd

---
+++ TddTestClass.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace NumberConveterProgram
+{
+    [TestFixture]
+    internal class TddTestClass
+    {
+        [Test]
+        public void Test1()
+        {
+            DecimalProgram DecimalToEverything = new DecimalProgram("10");
+            string result = DecimalToEverything.DecimalToHexa();
+            Assert.That(result, Is.EqualTo("12"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            DecimalProgram DecimalToEverything = new DecimalProgram("10");
+            string result = DecimalToEverything.DecimalToBinary();
+            Assert.That(result, Is.EqualTo("1010"));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            DecimalProgram DecimalToEverything = new DecimalProgram("10");
+            string result = DecimalToEverything.DecimalToOctal();
+            Assert.That(result, Is.EqualTo("12"));
+        }
+    }
+}
